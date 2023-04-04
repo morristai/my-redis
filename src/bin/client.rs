f43139dcb7c6f939250154f554ef24a6dc972fa7
Morris Tai <morristai01@gmail.com>
2023-04-04 02:10:02 -0400

    feat: add redis connection to receiver, use Command enum as msg type

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,23 +1,66 @@
+use bytes::Bytes;
+use mini_redis::client;
 use tokio::sync::mpsc;
+
+#[derive(Debug)]
+enum Command {
+    Get {
+        key: String,
+    },
+    Set {
+        key: String,
+        val: Bytes,
+    },
+}
 
 #[tokio::main]
 async fn main() {
     let (tx, mut rx) = mpsc::channel(32);
-    // It is not possible to clone the receiver of an mpsc channel.
+    // The `Sender` handles are moved into the tasks. As there are two
+    // tasks, we need a second `Sender`.
     let tx2 = tx.clone();
 
-    tokio::spawn(async move {
-        tx.send("sending from first handle").await;
+    // Spawn two tasks, one gets a key, the other sets a key
+    let t1 = tokio::spawn(async move {
+        let cmd = Command::Get {
+            key: "foo".to_string(),
+        };
+
+        tx.send(cmd).await.unwrap();
     });
 
-    tokio::spawn(async move {
-        tx2.send("sending from second handle").await;
+    let t2 = tokio::spawn(async move {
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            val: "bar".into(),
+        };
+
+        tx2.send(cmd).await.unwrap();
     });
 
-    // When every Sender has gone out of scope or has otherwise been dropped,
-    // it is no longer possible to send more messages into the channel. At this point,
-    // the recv call on the Receiver will return None, which means that all senders are gone and the channel is closed.
-    while let Some(message) = rx.recv().await {
-        println!("GOT = {}", message);
-    }
+    // spawn a task that processes messages from the channel. First, a client connection is established to Redis.
+    // Then, received commands are issued via the Redis connection.
+    let manager = tokio::spawn(async move {
+        // Establish a connection to the server
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+
+        // Start receiving messages
+        while let Some(cmd) = rx.recv().await {
+            use Command::*;
+
+            match cmd {
+                Get { key } => {
+                    client.get(&key).await;
+                }
+                Set { key, val } => {
+                    client.set(&key, val).await;
+                }
+            }
+        }
+    });
+
+    // .await the join handles to ensure the commands fully complete before the process exits
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
 }
